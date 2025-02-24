bec6d9687ba3e87a649e7f1a5cc6aa7c3ea5a720
thanhdnh <myhoangthanh@yahoo.com>
2025-02-24 08:17:15 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿public class Node{
+    public object data;
+    public Node next;  
+}
+public class MyStack{
+    Node top;
+    public bool IsEmpty(){
+        return top == null;
+    }
+    public void Push(object data){
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next = top;
+        top = newNode;
+    }
+    public object Pop(){
+        if(IsEmpty()){
+            return null;
+        }
+        object data = top.data;
+        top = top.next;
+        return data;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+        MyStack stack = new MyStack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        int v = (int)stack.Pop();
+        Console.WriteLine(v);
+    }
+}
